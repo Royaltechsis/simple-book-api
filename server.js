ee7baf295efba9ddc59fea24ae61ba7673114050
Oseni Oluwabunmi <osenibunmi2023@gmail.com>
2025-06-24 13:42:13 +0100

    cors installation

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
